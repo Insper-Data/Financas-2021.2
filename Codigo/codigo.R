e5bc82d0535582d781353b7c4a60572dce3a2dee
galupmor <88748156+galupmor@users.noreply.github.com>
2021-08-12 18:43:18 -0300

    Scraping e IBRX50

--- Codigo/codigo.R
+++ Codigo/codigo.R
@@ -1,9 +1,9 @@
-#########################################
 #Bibliotecas
 library(plyr)
 library(BatchGetSymbols)
 library(tidyquant)
 library(tidyverse)
+library(rvest)
 
 #Buscando ativos no IBOV
 ibov = GetIbovStocks()
@@ -28,3 +28,30 @@ portfolioReturns = na.omit(ROC(portfolioPrices))
 correl = cor(portfolioReturns)
 #Criando matriz de covariância dos retornos
 covar = cov(portfolioReturns)
+
+
+
+# Scraping para IBRX50 ----------------------------------------------------
+
+
+url = 'https://br.advfn.com/indice/ibrx-50'
+
+scraping_ibrx50 = url %>% 
+  read_html() %>%
+  html_nodes(".Column2") %>% 
+  html_text()
+
+scraping_ibrx50 = scraping_ibrx50[-1]
+scraping_ibrx50 = scraping_ibrx50[-length(scraping_ibrx50)]
+scraping_ibrx50[length(scraping_ibrx50)+1] = "AMER3"
+scraping_ibrx50 = sort(scraping_ibrx50)
+scraping_ibrx50_SA = paste(scraping_ibrx50,".SA",sep = "")
+cart = scraping_ibrx50_SA
+
+cart = ibov$tickersSA
+#Buscando dados pelo Yahoo
+portfolioPrices = NULL
+for(ticker in cart) {
+  portfolioPrices = cbind(portfolioPrices,
+                          getSymbols.yahoo(ticker, from = di, to= df, periodicity = "monthly", auto.assign = FALSE)[,4])
+}
